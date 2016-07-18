804f1644c86ed422352f3766311a4add9e6c01f3
Michael Hefferan <michael.hefferan@mydrivesolutions.com>
2016-07-18 15:04:44 +0100

    Make stdout test pass

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,14 @@
 class Fizzbuzz
+  attr_reader :lines_to_print
+
+  def initialize(lines_to_print)
+    @lines_to_print = lines_to_print
+  end
+
+  def print
+    1.upto(lines_to_print) do |i|
+      puts i
+    end
+  end
 end
 
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,6 +2,6 @@ require './fizzbuzz'
 
 describe Fizzbuzz do
   it 'outputs to stdout' do
-    expect(Fizzbuzz.new(100).print).to output.to_stdout
+    expect{ Fizzbuzz.new(100).print }.to output.to_stdout
   end
 end
